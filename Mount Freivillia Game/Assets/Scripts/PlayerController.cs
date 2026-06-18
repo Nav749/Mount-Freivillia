21c4f281352982aa5767335c5a46bb125fa5d7af
Nav749 <navingunaratnam77@gmail.com>
2026-06-18 14:58:48 -0400

    Made Player Controller
    Additionally made test scenes for everyone

---
+++ Mount Freivillia Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private float smoothTime = 0.05f;
+    private float _currentVelocity;
+    
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float _velocity;
+
+    [SerializeField] private float jumpPower;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
+
+    private void ApplyRotation()
+    {
+        if (_input.sqrMagnitude == 0) return;
+
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+    }
+
+    private void ApplyGravity()
+    {
+        if (isGrounded() && _velocity < 0.0f)
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityMultiplier * Time.deltaTime;
+        }
+        _direction.y = _velocity;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(!context.started) return;
+        if (!isGrounded()) return;
+
+        _velocity += jumpPower;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0, _input.y);
+    }
+
+    private bool isGrounded() => _characterController.isGrounded;
+}
